c67cd555648353aca0e613c9f93d7af0a5ff5c83
Gus <crawford.gus@gmail.com>
2018-08-29 00:17:35 -0400

    commit 0

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # fpm
-The Fake / Faux / Fugazzi Package Manager
+
+![FPM](fpm-logo.svg)
+
+## The Fake / Faux / Fugazzi Package Manager
+
+Manage interdependant packages that are under development; transform `package.json`'s for a working package into the package's `dist` folder.
+
+```
+yarn global add @guscrawford.com/fpm
+```
---
+++ dist/bin/main.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+const prog = require('caporal'), path = require('path'), fs = require('fs');
+prog
+    .version(require('./package.json').version)
+    .command('transform', 'Copy package.json and transform')
+        .argument('<directory>', 'path to app that will be packaged')
+        .argument('[directories...]', 'additional paths to package')
+        .option('--distFolder','the path to the output directory of your published package, \'dist\' by default')
+        .option('--dry','don\'t do anything, print the effect of the operation')
+            .action(distCommand)
+    .command('install','Copy a working package into another working package\'s node_modules folder')
+        .argument('<packagePath>', 'relative path to package to install in the node_modules of the current working directory')
+        .option('--dry','don\'t make any actual writes, print an effect report')
+        .action(installCommand);
+;
+prog.parse(process.argv);
+
+function transformCommand(args, options, logger) {
+
+    if (!args.directories) args.directories = [];
+    if (args.directory) args.directories.push(args.directory);
+    logger.info(options);
+    args.directories.forEach(createPackageJsonFor);
+    function createPackageJsonFor (projectPath) {
+        var absPath = path.resolve(projectPath);
+        logger.info(`ℹ️ Creating package.json for project in: ${absPath}`);
+        fs.lstat(absPath,function lstat_cb (error, info) {
+            //logger.info(info);
+            fs.readFile(`${path.join(absPath,'package.json')}`, {encoding:'utf8'}, transformPackage);
+        });
+        function transformPackage (error, packageJson) {
+            if (error) {
+                logger.error(error);
+                return error;
+            }
+            var package = JSON.parse(packageJson);
+            logger.info(`♻️  Transforming: ${path.join(absPath,'package.json')} to ${path.join(absPath,options.distFolder||'dist','package.json')}`)
+            if (options.dry) logger.info(package);
+            if (package["@package:transform"]) {
+                Object.keys(package["@package:transform"]).forEach(keyToTransform=>{
+                    switch(package["@package:transform"][keyToTransform]) {
+                        case '@package:remove':
+                            delete package[keyToTransform];
+                        break;
+                        default:
+                    }
+                    delete package["@package:transform"];
+                });
+                
+            }
+            if (options.dry) logger.info(`✅  Into:`);
+            if (options.dry) logger.info(package);
+            savePackage(JSON.stringify(package, null, '\t'), `${path.join(absPath, options.distFolder||'dist','package.json')}`);
+        }
+    }
+
+    function savePackage(packageData, packagePath) {
+        if (options.dry) logger.info(`ℹ️  writes to: ${packagePath}`);
+        else fs.writeFile(packagePath, packageData, 'utf8', (err,done)=>
+            err?logger.error(`❌  ${err}`):
+                logger.info(`📝  ${packagePath}`))
+    }
+}
+
+function installCommand(args, options, logger) {
+    logger.info(`ℹ️  Looking for node_modules in ${process.cwd()}`);
+}
