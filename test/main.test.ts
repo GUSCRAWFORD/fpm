3943eb7d3228d1a53a973f4339cdcc68d1642a14
Gus Crawford <gus.crawford@envisageny.com>
2019-01-08 10:35:51 -0500

    add support for tests

---
+++ test/main.test.ts
@@ -0,0 +1,17 @@
+
+
+import 'mocha';
+import { equal } from 'assert';
+import { InstallCommand } from '../src/install-command';
+import { TransformCommand } from '../src/transform-command';
+//const USERS_ROUTE = 'users';
+describe(`⚙️  Core Configs`, function (){
+    //console.info(routes);
+    let install = new InstallCommand(), transform = new TransformCommand();
+    it(`🔬 Unit Tests Work`, function () {
+        equal(
+            true
+            , true
+        );
+    });
+});
